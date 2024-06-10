e18d042e3102ba6b09a8de974d15d6c6461c7c80
Alex Ferreira <asfuture2019@gmail.com>
2024-06-10 10:51:37 -0300

    Adicionando Materialize-css
    Troca de biblioteca do Angular Material para o Materialize.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './componentes/header/header.component';
 import { HomeComponent } from './page/home/home.component';
 import { ContatoComponent } from './page/contato/contato.component';
 
 export const routes: Routes = [
-    {path:'',component:HeaderComponent},
+    {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:HomeComponent},
     {path:'contato',component:ContatoComponent},
 ];
--- src/app/page/contato/contato.component.ts
+++ src/app/page/contato/contato.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../componentes/header/header.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
 import { CarrosselComponent } from '../../componentes/carrossel/carrossel.component';
 import { TextoComponent } from '../../componentes/texto/texto.component';
+import { FormularioComponent } from '../../componentes/formulario/formulario.component';
+import { MenuComponent } from '../../componentes/menu/menu.component';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [ TextoComponent, CarrosselComponent,FooterComponent, HeaderComponent ],
+  imports: [MenuComponent, TextoComponent, CarrosselComponent,FooterComponent,FormularioComponent ],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
