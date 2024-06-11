2ee5d3f1ec406aa2230395f5de8257884da934bb
Alex Ferreira <asfuture2019@gmail.com>
2024-06-11 17:02:05 -0300

    Validação do formulário completa.
    Validação utilizando o Validatros e Ngx-Mask

--- src/app/componentes/formulario/formulario.component.ts
+++ src/app/componentes/formulario/formulario.component.ts
@@ -1,26 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { NgxMaskDirective,  } from 'ngx-mask';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [ReactiveFormsModule, ],
+  imports: [ReactiveFormsModule, CommonModule, NgxMaskDirective,],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  
+  constructor (private formBuilder:FormBuilder){}
+
   pedido = this.formBuilder.group({
-    nome: ['', Validators.required],
-    telefone: ['', Validators.required],
+    nome: ['', [Validators.required, Validators.maxLength(50)]],
+    telefone: ['', [Validators.required, Validators.maxLength(15)]],
     endereco: ['', Validators.required], 
-    mensagem: ['', Validators.required],
+    mensagem: ['', [Validators.required, Validators.maxLength(500)]],
  });
  
- constructor (private formBuilder:FormBuilder){}
-
  onSubmit(){
+  if(!this.pedido.valid){
+    console.log("Formulário inválido");
+    return;
+  }
   console.log(this.pedido.value);
  }
+
+//  get nome(){
+//   return this.pedido.get('nome')!;
+//  }
 }
 
