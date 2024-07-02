204ef4d092619ac65b4a91f17a9eac5410218268
Alex Ferreira <asfuture2019@gmail.com>
2024-07-01 22:45:22 -0300

    tela login

--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+import { login } from '../model/pedido.model';
+import { tap, Observable, catchError, of, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+    private readonly apiUrl:string = environment.apiUrl;
 
-  constructor() { }
-}
+    private messageSubject = new BehaviorSubject<string | null >(null);
+    message$ = this.messageSubject.asObservable();
+  
+    showMessage(message:string){
+      this.messageSubject.next(message);
+      setTimeout(() => this.messageSubject.next(null),3000)
+    }
+  
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  get(){
+        const login = this.http.get<login[]>(`${this.apiUrl}/login`,)
+        .pipe(
+          tap(() => {
+            this.showMessage("lista de pedidos obtida com sucesso!")
+          }),
+          catchError((error) => {
+            this.showMessage(`Erro ao obter listda de pedidos: ${error.message || error}`);
+            return of(null)
+          })
+      );
+        return login;
+      }
+  }
+
--- src/app/shared/views/page/login/login.component.ts
+++ src/app/shared/views/page/login/login.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../../services/login.service';
+import { login } from '../../../model/pedido.model';
+import { Subject, takeUntil } from 'rxjs';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -10,14 +14,40 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.component.css',
   providers:[HttpClient]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   login:any = FormGroup;
-
-  constructor( private formBuilder:FormBuilder ){}
-
+  private unsubscribe = new Subject<void>();
+  constructor( 
+    private formBuilder:FormBuilder,
+    private loginService:LoginService
+   ){}
+ngOnInit(): void {
+  this.login = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.maxLength(30)]],
+    senha: ['', [Validators.required, Validators.maxLength(4)]],
+ });
+}
   onSubmit() {
+   if(this.login.valid) {
+        const valor = this.login.value;
+          this.loginService.get().pipe(
+           takeUntil(this.unsubscribe))
+           .subscribe({
+             next: (response:login[] | null ) => {
+                 this.login = response;
+
+                 console.log("Api",this.login[0].email, this.login[0].senha )
+                 console.log("Login",valor.email, valor.senha )
+             }, 
+             error:(error) => {
+             console.log('Erro ao fazer requisição dos cards',error, )
+             }
+           })
+     this.login.reset();
+  }
+
     
-    }
 }
 
+}
