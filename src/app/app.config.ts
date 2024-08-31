15ae7215d478b555197b9cc5f515523f2b06e143
Alex Ferreira <asfuture2019@gmail.com>
2024-08-31 15:59:52 -0300

    Configurando imagem para hospedar no firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNgxMask } from 'ngx-mask';
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from './environments/environment';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNgxMask({}),provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideNgxMask({}),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideStorage(() => getStorage()), // Adiciona o Firebase Storage
+  ],
 };
